f1bc1c041d9e64bf344109aaa1b331dd03ff7441
Tim Hoskins <tim@thehoskins.net>
2018-12-21 04:21:32 -0700

    Updated to SidEx. Set up post-build event for GameData generation. Added Science AI.

---
+++ SidEx Project Manager/SidExProjectManager.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace SidEx.ProjectManager {
+	[KSPAddon(KSPAddon.Startup.Flight, false)]
+	public class SidExProjectManager : MonoBehaviour {
+		public const string DEBUGNAME = @"[SidEx Project Manager]";
+
+		public void Start() {
+			Debug.LogFormat(@"{0} Starting up...", DEBUGNAME);
+
+			GameEvents.onVesselChange.Add(this.VesselChangeHandler);
+			GameEvents.onVesselCreate.Add(this.VesselCreateHandler);
+			GameEvents.onGameStatePostLoad.Add(this.GameStatePostLoadHandler);
+
+			// we just loaded the flight scene, so we are in a vessel now .. run it
+			HandleVesselNaming(FlightGlobals.ActiveVessel);
+		}
+
+		public void OnDisable() {
+			Debug.LogFormat(@"{0} Disabling...", DEBUGNAME);
+			GameEvents.onVesselChange.Remove(this.VesselChangeHandler);
+			GameEvents.onVesselCreate.Remove(this.VesselCreateHandler);
+			GameEvents.onGameStatePostLoad.Remove(this.GameStatePostLoadHandler);
+		}
+
+		public void VesselChangeHandler(Vessel vessel) {
+//			Debug.LogFormat(@"{0} VesselChangeHandler()", DEBUGNAME);
+			HandleVesselNaming(vessel);
+		}
+
+		public void VesselCreateHandler(Vessel vessel) {
+//			Debug.LogFormat(@"{0} VesselCreateHandler()", DEBUGNAME);
+			HandleVesselNaming(vessel);
+		}
+
+		public void GameStatePostLoadHandler(ConfigNode node) {
+			if (HighLogic.LoadedSceneIsFlight && FlightGlobals.ActiveVessel != null)
+				HandleVesselNaming(FlightGlobals.ActiveVessel);
+		}
+
+		public void HandleVesselNaming(Vessel vessel) {
+			// if this got run before the scenario was created, don't do anything
+			if (SidExProjectManagerScenario.Instance == null)
+				return;
+
+			// if by some chance we don't have a vessel, just bug out
+			if (vessel == null)
+				return;
+
+			Debug.LogFormat(@"{0} Handling naming on {1}", DEBUGNAME, vessel.vesselName);
+
+			// set up shared numbering
+			Dictionary<string, int> sharedNumberingProjects = new Dictionary<string, int>();
+
+			// this would be a ship created without vesselnaming configured
+			//if (vessel.FindPartModulesImplementing<VesselNaming>().Count == 0) {
+			if (vessel.parts.FindAll(x => x.vesselNaming != null).Count == 0) { 
+				// handle unlikely events
+				if (string.IsNullOrEmpty(vessel.vesselName))
+					return;
+
+				string tag, countRoman;
+				if (!ConvertName(vessel.vesselName, ref sharedNumberingProjects, out tag, out countRoman))
+					return;
+
+				// apply the new name
+				vessel.vesselName = string.Format("{0} {1}", tag, countRoman);
+				Debug.LogFormat(@"{0} Renaming vessel to {1}", DEBUGNAME, vessel.vesselName);
+
+				// all done
+				return;
+			}
+
+//			for (int i = 0; i < vessel.parts.Count(); i++)
+//				Debug.LogFormat(@"{0} \tParts list: [{1}] {2} '{3}'", DEBUGNAME, i, vessel.parts[i].partName, vessel.parts[i].vesselNaming == null ? "null" : vessel.parts[i].vesselNaming.vesselName);
+
+			// we won't rename the vessel at the end if we don't modify it
+			bool hasBeenModified = false;
+
+			foreach (Part part in vessel.parts.FindAll(x => x.vesselNaming != null && x.vesselNaming.vesselName.Contains("["))) {
+				string partName = part.vesselNaming.vesselName;
+
+//				Debug.LogFormat(@"{0} Found part with vesselnaming {1} '{2}'", DEBUGNAME, part.partName, part.vesselNaming.vesselName);
+
+				// handle unlikely events
+				if (string.IsNullOrEmpty(partName))
+					continue;
+
+				string tag, countRoman;
+				if (!ConvertName(partName, ref sharedNumberingProjects, out tag, out countRoman))
+					continue;
+
+				// apply the new name to this part
+				part.vesselNaming.vesselName = string.Format("{0} {1}", tag, countRoman);
+
+				Debug.LogFormat(@"{0} Renaming tagged part {1} to {2}", DEBUGNAME, part.partName, part.vesselNaming.vesselName);
+
+				hasBeenModified = true;
+			}
+
+			// finally rename the whole vessel
+			if (hasBeenModified) {
+				vessel.vesselName = VesselNaming.FindPriorityNamePart(vessel).vesselNaming.vesselName;
+				Debug.LogFormat(@"{0} Renaming vessel to {1}", DEBUGNAME, vessel.vesselName);
+			}
+		}
+
+		private bool ConvertName(string partName, ref Dictionary<string, int> sharedNumberingProjects, out string tag, out string countRoman) {
+			tag = countRoman = string.Empty;
+
+			// set up shared numbering
+			bool sharedNumbering = partName.Contains("[[");
+
+			// get the tag
+			string tagRegex = @"\[([^\[]*?)\]"; // anything inside single or double square brackets
+			Match tagMatch = Regex.Match(partName, tagRegex);
+			if (string.IsNullOrEmpty(tagMatch.Value))
+				return false;
+
+			// clean up the tag
+			tag = tagMatch.Value;
+			tag = tag.Replace("[", "").Replace("]", "");
+
+			// get the project from the tag
+			string project = Regex.Replace(tag, @"\s+", "");
+			project = Regex.Replace(project, @"[^0-9a-zA-Z]+", "").ToUpper();
+
+
+			// look for an existing project and create if needed
+			ConfigNode projectNode = SidExProjectManagerScenario.Instance.node.GetNodes().Any(x => x.name == project) ? SidExProjectManagerScenario.Instance.node.GetNode(project) : SidExProjectManagerScenario.Instance.node.AddNode(project);
+
+			// get the count
+			int count;
+			if (!int.TryParse(projectNode.GetValue("count"), out count))
+				count = 0;
+
+			// check for shared numbering
+			if (sharedNumbering) {
+				if (!sharedNumberingProjects.ContainsKey(project)) { // the project is shared but not used yet, increment and save
+					count++;
+					sharedNumberingProjects.Add(project, count);
+				} else
+					count = sharedNumberingProjects[project];
+			} else
+				count++;
+
+			// convert to roman numerals
+			countRoman = ArabicToRoman(count);
+
+			// save the count
+			projectNode.SetValue("count", count.ToString(), true);
+
+			return true;
+		}
+
+		private string ArabicToRoman(int number) {
+			string[][] romanNumerals = new string[][] {
+				new string[]{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"}, // ones
+				new string[]{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"}, // tens
+				new string[]{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}, // hundreds
+				new string[]{"", "M", "MM", "MMM"} // thousands
+			};
+
+			// split integer string into array and reverse array
+			var intArr = number.ToString().Reverse().ToArray();
+			var len = intArr.Length;
+			var romanNumeral = "";
+			var i = len;
+
+			// starting with the highest place (for 3046, it would be the thousands
+			// place, or 3), get the roman numeral representation for that place
+			// and add it to the final roman numeral string
+			while (i-- > 0) {
+				romanNumeral += romanNumerals[i][Int32.Parse(intArr[i].ToString())];
+			}
+
+			return romanNumeral;
+		}
+	}
+}
---
+++ SidEx Project Manager/SidExProjectManagerScenario.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace SidEx.ProjectManager {
+
+	[KSPScenario(ScenarioCreationOptions.AddToAllGames, new GameScenes[] { GameScenes.FLIGHT })]
+	public class SidExProjectManagerScenario : ScenarioModule {
+		public const string DEBUGNAME = @"[SidEx Project Manager]";
+
+		// singleton
+		public static SidExProjectManagerScenario Instance { get; private set; }
+
+		private static ConfigNode _node; // where we're saving our project data in persistent.sfs
+		public ConfigNode node { get {
+				return _node;
+			} }
+
+		public override void OnAwake() {
+			base.OnAwake();
+
+			if (Instance == null)
+				Instance = this;
+		}
+
+		public override void OnLoad(ConfigNode node) {
+			Debug.LogFormat(@"{0} Loading data...", DEBUGNAME);
+			base.OnLoad(node);
+
+			if (Instance != this)
+				return;
+
+			if (node.HasNode("Projects"))
+				_node = node.GetNode("Projects");
+			else
+				_node = new ConfigNode("Projects");
+		}
+
+		public override void OnSave(ConfigNode node) {
+			Debug.LogFormat(@"{0} Saving data...", DEBUGNAME);
+			base.OnSave(node);
+
+			if (Instance != this)
+				return;
+
+			if (node.HasNode("Projects"))
+				node.RemoveNode("Projects");
+
+			node.AddNode(_node);
+		}
+	}
+}
