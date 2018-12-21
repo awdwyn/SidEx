03dd450f461cbf0559d55e3595193a57f44e9d32
Tim Hoskins <tim@thehoskins.net>
2018-12-21 05:44:17 -0700

    Added non-compiled mods

--- postbuild.bat
+++ postbuild.bat
@@ -4,13 +4,14 @@ rem post-build event: start /D "$(SolutionDir)" /WAIT postbuild.bat "$(SolutionD
 
 set PROJECT=%~5
 set PROJECT=%PROJECT: =%
+set MODDIR=%~1GameData (%~4)\SidEx\%PROJECT%\
 
-del /F /Q "%~2*.pdb"
-mkdir "%~2Plugins"
-move /Y "%~2*.dll" "%~2Plugins\"
-
-rmdir /S /Q "%~1GameData (%~4)\SidEx\%PROJECT%"
-mkdir "%~1GameData (%~4)\SidEx\%PROJECT%"
+rmdir /S /Q "%MODDIR%"
+mkdir "%MODDIR%"
 
 rem move "%~2" "%~1GameData (%~4)\SidEx\%PROJECT%"
-xcopy /E /Y "%~2*" "%~1GameData (%~4)\SidEx\%PROJECT%\"
+xcopy /E /Y "%~2*" "%MODDIR%"
+
+del /F /Q "%MODDIR%*.pdb"
+mkdir "%MODDIR%Plugins"
+move /Y "%MODDIR%*.dll" "%MODDIR%Plugins\"
